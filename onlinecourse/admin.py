8a011681ee5dade9274c652fd8a1f09979332dcb
jaedon <jaedon.visva2006@gmail.com>
2023-08-31 23:39:21 -0400

    initial

---
+++ onlinecourse/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 3
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 3
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+    list_display = ['title']
+
+
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
